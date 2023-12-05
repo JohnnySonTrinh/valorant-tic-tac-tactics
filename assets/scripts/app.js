40dd8f9075a1546262fc9aaab8459010b37e6736
Johnny Trinh <johnny.trinh@hotmail.se>
2023-12-05 07:22:13 +0100

    Add map event for mini-boards app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,20 @@
 const mapContainer = document.querySelector(".map-container")
 const map = document.querySelectorAll(".map")
 const back = document.querySelectorAll(".back")
+
+// Iterate over each map and click event
+map.forEach(map => {
+  map.addEventListener("click", () => {
+
+    // Get correspoding board ID
+    const boardId = "board" + map.id.replace("map", "")
+    // Hide all boards
+    document.querySelectorAll(".board").forEach(board => {
+      board.classList.add("hidden")
+    })
+    // Show the corresponding board
+    const board = document.getElementById(boardId)
+    board.classList.remove("hidden")
+    mapContainer.classList.add("hidden")
+  })
+})
