91b92adb37dba4c0fb4e46be257e9c4568fe5b59
Johnny Trinh <johnny.trinh@hotmail.se>
2023-12-07 13:38:00 +0100

    Add Bigwin and Win boards app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -90,6 +90,99 @@ function checkPlayerWin(player, winCombinations) {
     });
   });
 }
+
+// Function to check for an overall game win
+function checkOverallGameWin() {
+  const winCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+
+  // Debug: Check if the function is being called
+  console.log('Checking overall game win');
+
+  const razeWin = winCombinations.some(combination => {
+    return combination.every(index => {
+      const mapElement = document.querySelector(`.map[data-map="${index}"]`);
+      // Debug: Log the mapElement and its data-winner attribute
+      console.log(`Map ${index}:`, mapElement, mapElement.getAttribute('data-winner'));
+      return mapElement && mapElement.getAttribute('data-winner') === "Raze";
+    });
+  });
+
+  const cypherWin = winCombinations.some(combination => {
+    return combination.every(index => {
+      const mapElement = document.querySelector(`.map[data-map="${index}"]`);
+      // Debug: Log the mapElement and its data-winner attribute
+      console.log(`Map ${index}:`, mapElement, mapElement.getAttribute('data-winner'));
+      return mapElement && mapElement.getAttribute('data-winner') === "Cypher";
+    });
+  });
+
+  if (razeWin) {
+    console.log('Raze wins the overall game');
+    return "Raze";
+  } else if (cypherWin) {
+    console.log('Cypher wins the overall game');
+    return "Cypher";
+  }
+
+  console.log('No overall winner yet');
+  return null; // No overall winner yet
+}
+
+// Handles a cell click event
+function handleCellClick(cell, boardId) {
+  // Skip if the board already has a winner
+  if (boards[boardId].winner) return; 
+
+  const cellIndex = parseInt(cell.getAttribute('data-index'));
+  boards[boardId].cells[cellIndex] = currentPlayer; // Update the board's state
+
+  // Set cell content based on current player
+  cell.innerHTML = currentPlayer === "Raze" 
+    ? '<img class="raze" src="assets/images/ability-paint-shells.webp" alt="Raze">' 
+    : '<img class="cypher" src="assets/images/ability-spycam.webp" alt="Cypher">';
+
+  // Check if this move wins the game
+  if (checkWin(boards[boardId].cells, currentPlayer)) {
+    boards[boardId].winner = currentPlayer; // Set winner for the board
+    updateScore(currentPlayer); // Update score for the player
+    
+    // Get the corresponding map div
+    const mapIndex = boardId.replace('board', '');
+    const mapDiv = document.querySelector(`.map${mapIndex}`);
+    if (currentPlayer === "Raze") {
+      mapDiv.classList.add('raze-glow');
+      mapDiv.classList.remove('cypher-glow');
+    } else {
+      mapDiv.classList.add('cypher-glow');
+      mapDiv.classList.remove('raze-glow');
+    }
+    // Set data-winner attribute on the corresponding mini-board
+    const mapElement = document.querySelector(`.map[data-map="${boardId.replace('board', '')}"]`);
+    if (mapElement) {
+        mapElement.setAttribute('data-winner', currentPlayer);
+        console.log(`Winner set for map ${mapIndex}:`, mapElement.getAttribute('data-winner'));
+
+    }
+
+    alert(`${currentPlayer} wins on ${boardId}!`);
+
+    // Check for an overall winner across all boards
+    const overallWinner = checkOverallGameWin();
+    if (overallWinner) {
+      alert(`${overallWinner} wins the overall game!`);
+      endOverallGame(overallWinner); // Handle end of the overall game
+    }
+    lockBoard(boardId); // Lock the board as it has a winner
+  }
+
+  // Switch to the other player
+  currentPlayer = currentPlayer === "Raze" ? "Cypher" : "Raze";
+}
+
   } else {
     cell.innerHTML = '<img class="cypher" src="assets/images/ability-spycam.webp" alt="Cypher">';
     currentPlayer = "Raze";
